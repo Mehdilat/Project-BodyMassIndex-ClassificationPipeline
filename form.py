9358b0f6134d6e32ed5b2e4aa02400375cfad3de
Mahdi L. <58302359+Mehdilat@users.noreply.github.com>
2024-02-03 20:47:27 +0100

    Small Modifications

--- form.py
+++ form.py
@@ -8,9 +8,7 @@ min_weight = 40
 max_weight = 200
 
 class MetricForm(FlaskForm):
-    #height =  FloatField("Height", step=0.01, validators=[DataRequired()])
     gender = RadioField("Gender", choices = [(0, 'Male'), (1, 'Female')], validators=[InputRequired("Please choose an option")])
     height =  DecimalField("Height", places = 2, validators=[InputRequired("This field is required"), NumberRange(min = min_height, max = max_height, message="Value must be between 100 and 200")])
-    #weight =  FloatField("Height", step=0.1, validators=[DataRequired()])
     weight =  DecimalField("Weight", places = 1, validators=[InputRequired("This field is required"), NumberRange(min = min_weight, max = max_weight, message="Value must be between 100 and 200")])
     submit = SubmitField("Compute BMI")
